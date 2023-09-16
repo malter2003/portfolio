a9a104394dd8ebdfcb094a5f7cc21c9ea0ae445c
malter2003 <bdamja2003@gmail.com>
2023-09-16 15:31:09 -0600

    added uccs image

--- django_project/settings.py
+++ django_project/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
+import os
 
 from pathlib import Path
 
@@ -126,3 +127,13 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_URL = 'static/'
+
+
+STATICFILES_DIRS = [
+os.path.join(BASE_DIR, 'static')
+]
+
+
+MEDIA_URL = '/images/'
