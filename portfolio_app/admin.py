e8817441574dacfc622dbaf5ab6301b0153e880e
malter2003 <bdamja2003@gmail.com>
2023-10-03 00:27:24 -0600

    wip

--- portfolio_app/admin.py
+++ portfolio_app/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from .models import Student
+from .models import Portfolio
+from .models import Project
 
 
 # Register your models here so they can be edited in admin panel
 admin.site.register(Student)
+admin.site.register(Portfolio)
+admin.site.register(Project)
