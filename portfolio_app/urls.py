668d84f35c5d285417b1b34da3de8ef14ed408f1
malter2003 <bdamja2003@gmail.com>
2023-10-19 20:36:11 -0600

    ge 0 5

--- portfolio_app/urls.py
+++ portfolio_app/urls.py
@@ -6,5 +6,11 @@ urlpatterns = [
     path('students/', views.StudentListView.as_view(), name= 'students'),
     path('student/<int:pk>', views.StudentDetailView.as_view(), name='student-detail'),
     path('portfolios/', views.PortfolioListView.as_view(), name= 'portfolios'),
-    path('portfolio/<int:pk>', views.PortfolioDetailView.as_view(), name='portfolio-detail')
+    path('portfolio/<int:pk>', views.PortfolioDetailView.as_view(), name='portfolio-detail'),
+    path('projects/', views.ProjectListView.as_view(), name= 'projects'),
+    path('project/<int:pk>', views.ProjectDetailView.as_view(), name='project-detail'),
+    path('portfolio/<int:portfolio_id>/create_project/', views.createProject, name='create_project'),
+    path('project/<int:pk>/delete_project/', views.deleteProject, name='project-delete'),
+    path('project/<int:pk>/update_project/', views.updateProject, name='project-update'),
+    path('portfolio/<int:pk>/update_portfolio/', views.updatePortfolio, name='portfolio-update'),
 ]
--- portfolio_app/views.py
+++ portfolio_app/views.py
@@ -2,13 +2,79 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import *
 from django.views import generic
-#from .forms import ProjectForm, PortfolioForm
+from .forms import ProjectForm, PortfolioForm
+from django.shortcuts import get_object_or_404
 from django.contrib import messages
 
 def index(request):
     student_active_portfolios = Student.objects.select_related('portfolio').all().filter(portfolio__is_active=True)
     print("active portfolio query set", student_active_portfolios)
     return render( request, 'portfolio_app/index.html', {'student_active_portfolios':student_active_portfolios})
+
+def createProject(request, portfolio_id):
+    form = ProjectForm()
+    portfolio = Portfolio.objects.get(pk=portfolio_id)
+    
+    if request.method == 'POST':
+        # Create a new dictionary with form data and portfolio_id
+        project_data = request.POST.copy()
+        project_data['portfolio_id'] = portfolio_id
+        
+        form = ProjectForm(project_data)
+        if form.is_valid():
+            # Save the form without committing to the database
+            project = form.save(commit=False)
+            # Set the portfolio relationship
+            project.portfolio = portfolio
+            project.save()
+
+            # Redirect back to the portfolio detail page
+            return redirect('portfolio-detail', portfolio_id)
+
+    context = {'form': form}
+    return render(request, 'portfolio_app/project_form.html', context)
+
+def updateProject(request, pk):
+  project = get_object_or_404(Project, pk=pk)
+
+  if request.method == 'POST':
+    form = ProjectForm(request.POST, instance = project)
+    if form.is_valid():
+      form.save()
+      return redirect('project-detail', pk=pk)
+  else:
+    form = ProjectForm(instance=project)
+    context={
+     'form': form,
+     'project': project,
+    }
+  return render(request, 'portfolio_app/project_update.html', context)
+
+def updatePortfolio(request, pk):
+  portfolio = get_object_or_404(Portfolio, pk=pk)
+  
+  if request.method == 'POST':
+    form = PortfolioForm(request.POST, instance = portfolio)
+    if form.is_valid():
+      form.save()
+      return redirect('portfolio-detail', pk=pk)
+  else:
+    form = PortfolioForm(instance=portfolio)
+    context={
+     'form': form,
+     'portfolio': portfolio,
+    }
+  return render(request, 'portfolio_app/portfolio_update.html', context)
+
+def deleteProject(request, pk):
+    project = get_object_or_404(Project, pk=pk)
+    if request.method == 'POST':
+        project.delete()
+        # Redirect back to the portfolio detail page
+        return redirect('portfolio-detail', pk=project.portfolio.id)
+
+    context = {'project': project}
+    return render(request, 'portfolio_app/project_delete.html', context)
 
 # Create your views here.
 class StudentListView(generic.ListView):
@@ -19,4 +85,14 @@ class StudentDetailView(generic.DetailView):
 class PortfolioListView(generic.ListView):
     model = Portfolio
 class PortfolioDetailView(generic.DetailView):
-    model = Portfolio    
+    model = Portfolio   
+    def get_context_data(self, **kwargs):
+        context = super(PortfolioDetailView, self).get_context_data(**kwargs)
+        project_list = Project.objects.filter(portfolio_id=self.object)
+        context['project_list'] = project_list
+        return context 
+
+class ProjectListView(generic.ListView):
+    model = Project
+class ProjectDetailView(generic.DetailView):
+    model = Project       
